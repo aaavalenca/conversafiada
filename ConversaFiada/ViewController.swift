08c6d9ebc1197064ac984ebd358a8871a3001d78
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2022-05-16 18:05:16 -0300

    Icons and Autolayout

--- ConversaFiada/ViewController.swift
+++ ConversaFiada/ViewController.swift
@@ -9,23 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet var btnClean: RotatedButton!
-    
-    @IBOutlet var btnMeat: RotatedButton!
-    
-    @IBOutlet var btnCereal: RotatedButton!
-    
-    @IBOutlet var btnRefri: RotatedButton!
-    
-    @IBOutlet var btnBev: RotatedButton!
-    
-    @IBOutlet var btnFruit: RotatedButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        btnFruit.titleLabel?.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+
         
     }
     
